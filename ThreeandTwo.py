219a42e606a4cf80b185fd032f6333aea3e443f6
Graeme Acheson <graeme@sports4cast.com>
2023-12-20 21:21:11 +0000

    add in ml predictions

--- ThreeandTwo.py
+++ ThreeandTwo.py
@@ -7,6 +7,7 @@ from inputs.pull_sportsdb import get_CY_results
 from pipeline.format_table import format_results
 from outputs.to_gsheets import send_to_gsheet, clear_gsheet
 from pipeline.elos import get_elos
+from pipeline.ml_model import train_prediction_model, make_predictions
 
 location = os.getcwd()
 
@@ -18,25 +19,38 @@ with open(params_path, 'r') as file:
     best_parameters = json.load(file)
 
 
+
 #Get latest data
+print("Getting latest results")
 baseball_results = get_CY_results()
 
 
 #Format the dataframe
+print("Formatting")
 formatted_bb = format_results(baseball_results)
 
 
-
+#Run Elos
+print("Calculating rankings")
 df = pd.read_csv(file_path)
 elo_ranks, elo_matches = get_elos(df, best_parameters)
 
 
+#ML Model
+print("Running predictions")
+model = train_prediction_model(elo_matches)
+preds = make_predictions(model, elo_matches)
+
+
 #Send to gsheets
+print("Uploading")
 clear_gsheet("threeandTwo Baseball Data", "Results")
 clear_gsheet("threeandTwo Baseball Data", "Rankings")
 clear_gsheet("threeandTwo Baseball Data", "Rankings by Match")
 
 send_to_gsheet(formatted_bb, "threeandTwo Baseball Data", "Results")
 send_to_gsheet(elo_ranks, "threeandTwo Baseball Data", "Rankings")
-send_to_gsheet(elo_matches, "threeandTwo Baseball Data", "Rankings by Match")
+send_to_gsheet(preds, "threeandTwo Baseball Data", "Rankings by Match")
 
+
+print("Complete")
--- pipeline/ml_model.py
+++ pipeline/ml_model.py
@@ -3,6 +3,25 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 from datetime import datetime
+import numpy as np
+
+def calculate_prediction(row):
+    if pd.isnull(row['win']) or pd.isnull(row['loss']) or row['score_pt1'] == 'Cancelled':
+        return 99
+    elif float(row['win']) > float(row['loss']):
+        return 1
+    elif float(row['win']) < float(row['loss']):
+        return 0
+    else:
+        return 0.5
+    
+
+def compare_result_and_prediction(result, prediction):
+    if result.equals(99):  # Use .equals() for Series comparison
+        return None
+    else:
+        return np.equal(result, prediction)
+
 
 def train_prediction_model(df):
     df = df[df['result'] != 99]
@@ -11,7 +30,7 @@ def train_prediction_model(df):
     df.loc[:, 'result'] = df['result'].map(result_mapping)
 
     # Create a feature matrix X and target variable y
-    X = df[['elo_team1', 'elo_team2', 'home_rest_days', 'away_rest_days']]
+    X = df[['elo_team1', 'elo_team2']]
     y = df['result'].astype(int)
 
     # Split the data into training and testing sets
@@ -37,12 +56,16 @@ def make_predictions(model, df):
     # upcoming_df = df[df['date'] >= now]
     upcoming_df = df.copy()
 
-    probabilities = model.predict_proba(upcoming_df[['elo_team1', 'elo_team2', 'home_rest_days', 'away_rest_days']])
+    probabilities = model.predict_proba(upcoming_df[['elo_team1', 'elo_team2']])
     
     upcoming_df = upcoming_df.copy()
     upcoming_df['win'] = probabilities[:, 2]
     upcoming_df['draw'] = probabilities[:, 1] 
     upcoming_df['loss'] = probabilities[:, 0]
+
+    upcoming_df['pred'] = upcoming_df.apply(calculate_prediction, axis=1)
+
+    upcoming_df['correct_prediction'] = compare_result_and_prediction(upcoming_df['result'], upcoming_df['pred'])
     
     # df = df[df['date'] < now]
 
